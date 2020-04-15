724567b99a5df0a18824c45fb16d862f9712ab42
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-04-15 09:51:17 +0200

    feat(button): add button

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({
+  onClick,
+  children,
+}) => (
+  <button onClick={onClick} type="button">
+    {children}
+  </button>
+);
+
+export default Button;
