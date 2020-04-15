cbaaa0c981655f97be1dcda1f6b237ca8adaf689
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-04-15 10:22:48 +0200

    feat(app): get data from unsplash api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
+
 import './App.css';
 
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       keywords: null,
+      images: [],
     };
   }
 
   setKeywords = (keywords) => this.setState({ keywords });
 
   search = () => {
-    console.log('hello');
+    Axios.get(`https://api.unsplash.com/search/photos?query=${this.state.keywords}&client_id=gS4yjdOF7bCy1YmWS5bGdOo-kjy520FRokEHcs8w48M`)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ images: data.results.map((result) => result.urls.regular) });
+      });
   }
 
   render() {
+    const { images } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <SearchBar setKeywords={this.setKeywords} onSearch={this.search} />
+          {
+            images.map((image) => <img src={image} alt="" />)
+          }
         </header>
       </div>
     );
