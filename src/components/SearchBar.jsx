b00a4abcc8b53ac2043b0d2b2aebd1d1f19bb486
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-04-15 09:54:19 +0200

    refactor(button): use button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Button from './Button';
 import TextInput from './TextInput';
 
 const SearchBar = ({
   setKeywords,
+  onSearch,
 }) => (
-  <TextInput setText={setKeywords} />
+  <>
+    <TextInput setText={setKeywords} />
+    <Button onClick={onSearch}>
+      Recherche
+    </Button>
+  </>
 );
 
 export default SearchBar;
